12c26d01a877b4a7795f0252981fd2b5548e43a1
edthuman <edwardadirvine@aol.com>
2024-07-11 23:09:26 +0100

    reduce size of logs.ts

--- db/data/test-data/logs.ts
+++ db/data/test-data/logs.ts
@@ -5,16 +5,6 @@ module.exports = [
         date: "2024-06-24"
     },
     {
-        username: "laa-laa",
-        habit: "saying eh-oh",
-        date: "2024-06-24"
-    },
-    {
-        username: "po",
-        habit: "saying eh-oh",
-        date: "2024-06-24"
-    },
-    {
         username: "dipsy",
         habit: "saying eh-oh",
         date: "2024-06-25"
@@ -30,11 +20,6 @@ module.exports = [
         date: "2024-06-26"
     },
     {
-        username: "laa-laa",
-        habit: "saying eh-oh",
-        date: "2024-06-26"
-    },
-    {
         username: "po",
         habit: "saying eh-oh",
         date: "2024-06-26"
@@ -43,45 +28,10 @@ module.exports = [
         username: "dipsy",
         habit: "saying eh-oh",
         date: "2024-06-27"
-    },
-    {
-        username: "laa-laa",
-        habit: "saying eh-oh",
-        date: "2024-06-27"
-    },
-    {
-        username: "dipsy",
-        habit: "saying eh-oh",
-        date: "2024-06-28"
-    },
-    {
-        username: "po",
-        habit: "saying eh-oh",
-        date: "2024-06-28"
-    },
-    {
-        username: "dipsy",
-        habit: "cleaning",
-        date: "2024-06-29"
-    },
-    {
-        username: "dipsy",
-        habit: "saying eh-oh",
-        date: "2024-06-29"
     },
     {
         username: "laa-laa",
         habit: "exercising",
-        date: "2024-06-29"
-    },
-    {
-        username: "laa-laa",
-        habit: "saying eh-oh",
-        date: "2024-06-29"
-    },
-    {
-        username: "po",
-        habit: "saying eh-oh",
         date: "2024-06-29"
     },
     {
@@ -90,18 +40,8 @@ module.exports = [
         date: "2024-06-30"
     },
     {
-        username: "dipsy",
-        habit: "saying eh-oh",
-        date: "2024-06-30"
-    },
-    {
         username: "laa-laa",
         habit: "exercising",
-        date: "2024-06-30"
-    },
-    {
-        username: "laa-laa",
-        habit: "saying eh-oh",
         date: "2024-06-30"
     },
     {
