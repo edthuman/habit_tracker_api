bc9a0cd99e7ec6100a6dd40e877f6a5abbc5233f
edthuman <edwardadirvine@aol.com>
2024-07-12 19:15:25 +0100

    update expected structure of logsData

--- db/data/test-data/index.ts
+++ db/data/test-data/index.ts
@@ -1,6 +1,6 @@
 const usersData: [{ username: string }] = require("./users")
 const habitsData: [{ slug: string, description: string }] = require("./habits")
-const usersHabitsData: [{ username: string, habit: string }] = require("./users-habits")
+const usersHabitsData: [{ user: string, habit: string }] = require("./users-habits")
 const logsData: [{ username: string, habit: string, date: string }] = require("./logs")
 
 module.exports = { usersData, habitsData, usersHabitsData, logsData }
