b5cd680641370fc08bee76ceb769f7603d29b06a
edthuman <edwardadirvine@aol.com>
2024-07-12 19:17:28 +0100

    add production data for seeding

---
+++ db/data/production-data/habits.ts
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        slug: "quit smoking",
+        description: "not smoking any cigarettes"
+    },
+    {
+        slug: "exercising",
+        description: "doing a set amount of exercise"
+    },
+    {
+        slug: "journaling",
+        description: "writing down thoughts/feelings"
+    },
+    {
+        slug: "reading",
+        description: "reading a book or listening to an audiobook"
+    },
+    {
+        slug: "waking up early",
+        description: "waking up early in the morning"
+    }
+]
---
+++ db/data/production-data/index.ts
@@ -0,0 +1,8 @@
+const usersData: [{ username: string }] = require("./users")
+const habitsData: [{ slug: string, description: string }] = require("./habits")
+const usersHabitsData: [{ user: string, habit: string }] = require("./users-habits")
+const logsData: [{ user: string, habit: string, date: string }] = require("./logs")
+
+module.exports = { usersData, habitsData, usersHabitsData, logsData }
+
+export {}
